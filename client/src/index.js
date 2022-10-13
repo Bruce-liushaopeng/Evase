35d9ca295146a197270e6326d938fd508a5b6159
bruce <bruceliu1107@gmail.com>
2022-10-13 17:38:08 -0400

    remove redux boilerplate code

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
 import EvaseApp from './EvaseApp';
 import reportWebVitals from './reportWebVitals';
-import './index.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <EvaseApp />
-    </Provider>
+    <EvaseApp />
   </React.StrictMode>
 );
 
