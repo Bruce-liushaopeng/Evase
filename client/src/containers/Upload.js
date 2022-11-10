5e0eb9ceaaaca2f1134d7bc68b8f0d17c9837d53
bruce <bruceliu1107@gmail.com>
2022-11-10 12:44:30 -0500

    drag and drop

--- client/src/containers/Upload.js
+++ client/src/containers/Upload.js
@@ -1,19 +1,12 @@
 import React, { useState } from 'react'
-
-const axios = require('axios').default;
+import { DropzoneDialog } from "material-ui-dropzone"
+import { Button } from 'ui-neumorphism'
 
 const Upload = (props) => {
 
     const [selectedFile, setSelectedFile] = useState();
     const [isFilePicked, setIsFilePicked] = useState(false)
-
-     const changeHandler = (e) => {
-        e.preventDefault()
-        props.onChange()
-        setSelectedFile(e.target.files[0]);
-        setIsFilePicked(true)
-        console.log("Change to file handled.")
-     }
+    const [dialogOpen, setDialogOpen] = useState(false)
 
     const cancelFile = (e) => {
         e.preventDefault()
@@ -28,35 +21,61 @@ const Upload = (props) => {
         props.onSubmission(selectedFile)
     }
 
+    const showDialog = () => {
+        setDialogOpen(true)
+    }
+
+    const handleSave = (files) => {
+        setDialogOpen(false)
+        setSelectedFile(files[0])
+        setIsFilePicked(true)
+        console.log(files)
+    }
+
+    const handleClose = () => {
+        setDialogOpen(false)
+    }
+
 
     return (
-    <div>
-        <header className="uploadInstruction">
-            <p>{props.instruction}</p>
-        </header>
-        <input type="file" name="file" onChange={changeHandler} />
-        {isFilePicked ? (
-            <div>
-                <p>Filename: {selectedFile.name}</p>
-                <p>Filetype: {selectedFile.type}</p>
-                <p>Size in bytes: {selectedFile.size}</p>
-                <p>
-                    Date last modified:{' '}
-                    {selectedFile.lastModifiedDate.toLocaleDateString()}
-                </p>
-            </div>
-        ) : (
-            <p>Select a file to show details</p>
-        )}
-        {isFilePicked ? (
-            <div>
-                <button onClick={handleSubmission}>Upload!</button>
-                <button onClick={cancelFile}>Cancel</button>
-            </div>
-        ) :
-            <div/>
-        }
-	</div>
+        <div>
+            <header className="uploadInstruction">
+                <p>{props.instruction}</p>
+            </header>
+            <Button onClick={showDialog} >
+                Select file here
+            </Button>
+            <DropzoneDialog
+                open={dialogOpen}
+                onSave={handleSave}
+                showPreviews={true}
+                onClose={handleClose}
+                submitButtonText="Confirm"
+                filesLimit={1}
+                maxFileSize={500000000}
+            />
+            {isFilePicked ? (
+                <div>
+                    <p>Filename: {selectedFile.name}</p>
+                    <p>Filetype: {selectedFile.type}</p>
+                    <p>Size in bytes: {selectedFile.size}</p>
+                    <p>
+                        Date last modified:{' '}
+                        {selectedFile.lastModifiedDate.toLocaleDateString()}
+                    </p>
+                </div>
+            ) : (
+                <p>Select a file to show details</p>
+            )}
+            {isFilePicked ? (
+                <div>
+                    <button onClick={handleSubmission}>Upload!</button>
+                    <button onClick={cancelFile}>Cancel</button>
+                </div>
+            ) :
+                <div />
+            }
+        </div>
     )
 }
 
