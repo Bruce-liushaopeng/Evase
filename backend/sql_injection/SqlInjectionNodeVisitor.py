0ef8155e556efe2a5f396c9038f5d9b3a68e2669
Tony Zeidan <tony.azp25@gmail.com>
2022-11-24 16:51:43 -0500

    Revert back to the model without `ModuleAnalysisStruct`

--- backend/sql_injection/SqlInjectionNodeVisitor.py
+++ backend/sql_injection/SqlInjectionNodeVisitor.py
@@ -1,17 +1,9 @@
 import ast
 from pprint import pprint
-from ..depanalyze.modulestructure import ModuleAnalysisStruct
 
 class SqlInjectionNodeVisitor(ast.NodeVisitor):
     # cursor_name = None
     # sql_package_names = ["sqlite3", "mysql"]
-
-    def __init__(self, analysis_struct: ModuleAnalysisStruct):
-        self.analysis_structure = analysis_struct
-        self.process()
-
-    def process(self):
-        self.generic_visit(self.analysis_structure.get_ast())
 
     def assign_parent_nodes(self, root_module:ast.Module):
         setattr(root_module, 'parent', None)
