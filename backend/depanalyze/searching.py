680750e9bacfb592b30bd2b73a8977d545bea2e4
Tony Zeidan <tony.azp25@gmail.com>
2022-12-02 19:53:03 -0500

    Comments for Bruce and Anthony.

--- backend/depanalyze/searching.py
+++ backend/depanalyze/searching.py
@@ -7,11 +7,14 @@ from backend.depanalyze.projectstructure import ProjectAnalysisStruct
 def get_function_module_uses(func_node: ast.FunctionDef, prj_struct: ProjectAnalysisStruct):
     name = func_node.name
     mdls = []
+
+    # check project structure for any modules that import this function
     for k, v in prj_struct.get_module_structure().items():
         for imp, (imp_mdl, imp_name) in v.get_module_imports():
             if name == imp_name:
                 mdls.append(imp_mdl)
 
+    # for each module that uses this function, retrieve the functions that use this function
     funcs = []
     for mdl in mdls:
         fns = []
@@ -30,6 +33,8 @@ def get_function_module_uses(func_node: ast.FunctionDef, prj_struct: ProjectAnal
 def get_function_call_origin(func_node: ast.Call, mdl_struct: ModuleAnalysisStruct):
     fn_name = func_node.func.id
     mdls = []
+    
+    # given a function node, find the module it comes from with the dependency graph in ModuleAnalysisStruct
     for imp, (imp_mdl, imp_name) in mdl_struct.get_module_imports():
         if fn_name == imp_name:
             mdls.append(imp_mdl)
