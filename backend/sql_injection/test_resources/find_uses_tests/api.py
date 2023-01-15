36a9c06a9b19af007b6f085299f685480116c5c9
AnthonyDooley <anthonylouisdooley@gmail.com>
2023-01-14 21:16:16 -0500

    fix of import

--- backend/sql_injection/test_resources/find_uses_tests/api.py
+++ backend/sql_injection/test_resources/find_uses_tests/api.py
@@ -1,9 +1,9 @@
 from flask import Flask
 
-from test_resources.find_uses_tests.second import lastAPI
+from find_uses_tests.second import lastAPI
 
 app = Flask(__name__)
 
 @app.route('/upload', methods=['POST'])
 def file_upload_hook(a):
-    lastAPI(a)
+    lastAPI("a")
