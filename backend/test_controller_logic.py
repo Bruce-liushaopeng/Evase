667921e6761b02e56b665ba57abfe2efa3d58151
Tony Abou-Zeidan <tony.azp25@gmail.com>
2023-01-19 16:57:02 -0500

    fixer

--- backend/test_controller_logic.py
+++ backend/test_controller_logic.py
@@ -1,12 +1,13 @@
 from backend.controller_logic import perform_analysis
 import os
 
-def main():
 
+def main():
     path_here = os.path.dirname(os.path.realpath(__file__))
     demo_code = os.path.join(path_here, 'test', 'resources', 'demonstration')
 
     perform_analysis(demo_code, demo_code, sql_injection=True)
+
 
 if __name__ == '__main__':
     main()
