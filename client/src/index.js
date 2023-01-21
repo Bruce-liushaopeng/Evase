39cae642888cff6485e50a4e9797500bd220b5fc
bruce <bruceliu1107@gmail.com>
2023-01-21 16:15:47 -0500

    uiNov10

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import EvaseApp from './EvaseApp';
 import reportWebVitals from './reportWebVitals';
 
-
 const container = document.getElementById('root');
 const root = createRoot(container);
 
