30accab0f9d637c06a3a9aeba670bad1844438b0
AnthonyDooley <anthonylouisdooley@gmail.com>
2023-01-21 12:16:24 -0500

    most recent

--- backend/test/resources/demo/backend/app.py
+++ backend/test/resources/demo/backend/app.py
@@ -4,15 +4,13 @@ from vul import newUserToDB, getUserFromDB
 app = Flask(__name__)
 CORS(app)
 
-@app.route('/addUser', methods=['POST'], strict_slashes=False)
-def addUser():
-    firstName = request.json['firstName']
-    lastName = request.json['lastName']
-    print(firstName, lastName)
-    response = newUserToDB(firstName, lastName)
-    return response
+@app.route('/addUser/<userName>/<password>', methods=['POST'], strict_slashes=False)
+def addUser(userName=None, password=None):
+    print(userName, password)
+    newUserToDB(userName, password)
+    return "ok"
 
-@app.route('/getUser', methods=['GET'], strict_slashes=False)
-def getUser(firstName):
-    response = getUserFromDB(firstName)
-    return response
+@app.route('/getUser/<userName>', methods=['GET'], strict_slashes=False)
+def getUser(userName=None):
+    respond = getUserFromDB(userName)
+    return respond
